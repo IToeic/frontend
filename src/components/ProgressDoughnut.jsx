108f7c4c74a4bea406f9fa45f14b650a32a2d53f
quequuen <rkdusl2020@gmail.com>
2025-07-26 17:52:42 +0900

    Fix: DashBoard 내 차트 react-chartjs-2 chart.js 라이브러리 이용한 차트로 변경

---
+++ src/components/ProgressDoughnut.jsx
@@ -0,0 +1,35 @@
+import { Doughnut } from "react-chartjs-2";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+
+// Chart.js에 필요한 요소 등록
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+const ProgressDoughnut = () => {
+  const data = {
+    labels: ["완료", "남은 것"],
+    datasets: [
+      {
+        data: [4, 1], // 학습한 개수 4, 남은 개수 1
+        backgroundColor: ["#3B82F6", "#E5E7EB"],
+        borderWidth: 0,
+      },
+    ],
+  };
+
+  const options = {
+    cutout: "50%", // 이 부분이 도넛 비율
+    plugins: {
+      legend: {
+        display: false, // 범례 숨기기
+      },
+    },
+  };
+
+  return (
+    <div className="w-32 h-32">
+      <Doughnut options={options} data={data} />
+    </div>
+  );
+};
+
+export default ProgressDoughnut;
--- src/pages/DashBoard.jsx
+++ src/pages/DashBoard.jsx
@@ -1,3 +1,5 @@
+import ProgressDoughnut from "../components/ProgressDoughnut";
+
 const DashBoard = () => {
   return (
     <div className="flex-1 bg-white p-10">
@@ -8,8 +10,11 @@ const DashBoard = () => {
       <div className="mb-4 text-lg">
         <span role="img" aria-label="folder">
           📁
-        </span>{" "}
+        </span>
         진행중인 단어팩: <span className="font-semibold">초급 어쩌구</span>
+        <button className="bg-blue-600 text-white px-2 py-1 ml-3 rounded hover:bg-blue-700 text-sm">
+          변경
+        </button>
       </div>
 
       {/* 오늘의 진행상황 */}
@@ -19,12 +24,14 @@ const DashBoard = () => {
         <div className="bg-blue-100 p-6 rounded shadow-md flex items-center gap-6">
           {/* 왼쪽 원형 차트 & 숫자 */}
           <div className="text-center">
-            <div className="w-24 h-24 rounded-full bg-blue-600 mx-auto mb-2"></div>
-            <p className="font-semibold text-sm">4 / 5</p>
+            <div className="text-center">
+              <ProgressDoughnut />
+              <p className="font-semibold text-sm mt-2">4 / 5</p>
+            </div>
           </div>
 
           {/* 오른쪽 텍스트 */}
-          <div className="flex-1 space-y-1 text-sm">
+          <div className="flex-1 space-y-1 text-sm ml-10">
             <p>
               📌 오늘 배운 단어: <span className="font-semibold">4개</span>
             </p>
